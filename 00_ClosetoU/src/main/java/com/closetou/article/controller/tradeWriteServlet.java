dace4ab7ee00b1c94c47c583daab23bec87cc693
이정준 <ljj0210@naver.com>
2023-02-22 18:43:20 +0900

    0222 작업

--- 00_ClosetoU/src/main/java/com/closetou/article/controller/tradeWriteServlet.java
+++ 00_ClosetoU/src/main/java/com/closetou/article/controller/tradeWriteServlet.java
@@ -8,9 +8,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.closetou.article.model.service.ArticleService;
+import com.closetou.article.model.vo.Article;
 import com.closetou.board.model.service.BoardService;
 import com.closetou.cloth.model.vo.ClothCategory;
+import com.closetou.common.util.FileRename;
+import com.closetou.member.model.vo.Member;
+import com.oreilly.servlet.MultipartRequest;
 
 @WebServlet(name = "tradewrite", urlPatterns = { "/article/tradeWrite" })
 public class tradeWriteServlet extends HttpServlet {
@@ -23,6 +29,8 @@ public class tradeWriteServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		//로그인 체크 필터 필요
+		
 		ArrayList<ClothCategory> categorylist = new ArrayList<>();
 
 		categorylist = new BoardService().getClothCategories();
@@ -35,6 +43,63 @@ public class tradeWriteServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		// 첨부파일 넣으면 서버로 전달
+
+		/*
+		 * // 사용자가 보낸 파라미터값 받기 System.out.println(request.getParameter("title")); //
+		 * wirte.jsp의 50행 name System.out.println(request.getParameter("writer"));
+		 * System.out.println(request.getParameter("upfile")); // 이렇게 하면 파일을 보내는 게 아니라
+		 * 파일명만 보냄 System.out.println(request.getParameter("content"));
+		 */
+
+//		    	MultipartRequest mr = new MultipartRequest(request, getServletName(), 0, getServletInfo(), null);
+
+		HttpSession session = request.getSession(false);
+		Member loginMember = (session == null) ? null : (Member) session.getAttribute("loginMember");
+
+		if (loginMember != null) { // 로그인 상태
+
+			// 파일이 저장될 경로 얻어오기
+			String path = getServletContext().getRealPath("/resources/boardUpfile"); // / = 현재 웹 애플리케이션에서 webapp에 해당
+
+			// 파일의 최대 사이즈 지정(10MB로 지정)
+			// 사이즈 지정은 바이트 단위로 한다. 10485760byte
+			int maxSize = 10485760;
+
+			// 파일 인코딩 설정
+			String encoding = "UTF-8";
+
+	
+			MultipartRequest mr = new MultipartRequest(request, path, maxSize, encoding, new FileRename());
+
+			Article article = new Article();
+
+			article.setUserNo(loginMember.getNo());
+			article.setUserNickname(loginMember.getNickname());
+			article.setTitle(mr.getParameter("title"));
+			article.setContent(mr.getParameter("content"));
+			article.setRenamedFileName(mr.getFilesystemName("upfile")); // 클라이언트가 파일을 선택해서 올린 이름 ex) 정처기해설집
+			article.setOriginalFileName(mr.getOriginalFileName("upfile")); // 실제 서버에 업로드(저장) 될 때 파일 이름 ex)
+			// 두 개의 파일명을 다 저장해야 함. 서버 저장용, 클라이언트에게 다시 보낼 때 사용하는 용
+			article.setType(mr.getParameter("type"));
+
+			int result = new ArticleService().save(article);
+
+			if (result > 0) { // 게시글 작성 성공
+				request.setAttribute("msg", "게시글 등록 성공.");
+				request.setAttribute("location", "/board/communityBoardList");
+			} else { // 게시글 작성 실패
+				request.setAttribute("msg", "게시글 등록 실패.");
+				request.setAttribute("location", "/board/communityBoardList");
+			}
+
+		} else { // 로그인 미상태
+			request.setAttribute("msg", "로그인 후 수정해 주세요.");
+			request.setAttribute("location", "/");
+		}
+
+		request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
+
 	}
 
 }
--- 00_ClosetoU/src/main/webapp/views/board/tradeBoardWrite.jsp
+++ 00_ClosetoU/src/main/webapp/views/board/tradeBoardWrite.jsp
@@ -13,52 +13,46 @@
 			page="/views/common/sidemenu/tradeSideMenu.jsp" /></div>
 	<article id="content">
 		<div id='board-write-container'>
-			<div id = "submenuarea">
-			<h2>거래 글 등록</h2>
+			<div id="submenuarea">
+				<h2>거래 글 등록</h2>
 			</div>
 			<form action="${ path }/ariticle/communityWrite" method="POST"
 				enctype="multipart/form-data">
 				<div id="cloth_enroll">
-					<input type="text" name="cloth_name">
-					<button id="togglecategory_btn" type="button">카테고리 열기</button>
-					<div id = "categoryoptions">
-						<c:forEach var="category" items="${ categorylist }">
-							<c:choose>
-								<c:when test="${ category.clothCode == '00' }">
-									<input type="checkbox" name="clothcategory"
-										value="${ category.clothCategory }" checked>${ category.clothCategory }						
+					<div id="photoarea">
+						<input type="file" name="upfile">
+					</div>
+				</div>
+				<div>
+					<input type="text" name="cloth_name" placeholder="상품명을 입력해주세요.">
+					<button id="togglecategory_btn" type="button">카테고리 등록</button>
+				</div>
+				<div id="categoryoptions">
+					<c:forEach var="category" items="${ categorylist }">
+						<c:choose>
+							<c:when test="${ category.clothCode == '00' }">
+								<input type="checkbox" name="clothcategory"
+									value="${ category.clothCategory }" checked>${ category.clothCategory }						
 								</c:when>
-								<c:otherwise>
-									<input type="checkbox" name="clothcategory"
-										value="${ category.clothCategory }">${ category.clothCategory }
+							<c:otherwise>
+								<input type="checkbox" name="clothcategory"
+									value="${ category.clothCategory }">${ category.clothCategory }
 								
 								</c:otherwise>
-							</c:choose>
-						</c:forEach>
-					</div>
-
+						</c:choose>
+					</c:forEach>
 				</div>
-
 				<div id="content">
-					<table id='tbl-board'>
-						<tr>
-							<th>제목</th>
-							<td><input type="text" name="title" id="title"></td>
-						</tr>
-						<tr>
-							<th>작성자</th>
-							<td><input type="text" name="writer"
-								value="${ loginMember.id }" readonly></td>
-						</tr>
-						<tr>
-							<th>내용</th>
-							<td><textarea name="content" cols="50" rows="15"></textarea></td>
-						</tr>
-						<tr>
-							<th colspan="2"><input type="submit" value="등록"> <input
-								type="reset" value="취소"></th>
-						</tr>
-					</table>
+					<div>
+						<input type="text" name="title" id="title" placeholder="제목">
+					</div>
+					<div>
+						<textarea name="content" cols="50" rows="15"></textarea>
+					</div>
+					<div>
+						<input type="submit" value="등록"> <input type="reset"
+							value="취소">
+					</div>
 				</div>
 			</form>
 		</div>
